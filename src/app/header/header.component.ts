3de05cf8683748e0621ce74c73e00bca6e3a3a81
manar <manar.jennene@esprit.tn>
2023-12-05 20:38:50 +0100

    template

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: [
+    './assets/vendor/bootstrap/css/bootstrap.min.css',
+    './assets/vendor/quill/quill.snow.css',
+    './assets/vendor/quill/quill.bubble.css',
+    './assets/vendor/remixicon/remixicon.css',
+    './assets/vendor/simple-datatables/style.css',
+    './assets/css/style.css'
+  ]
+  })
+export class HeaderComponent {
+
+}
